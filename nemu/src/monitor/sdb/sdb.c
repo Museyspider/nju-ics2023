9e0cea73aa0431d005239cd788b780c462b92b0a
tracer-ics2023 <tracer@njuics.org>
2023-12-17 01:17:28 -0500

    >  compile NEMU QSX20210747 unian Linux unian-QiTianM415-D412 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:17:28 up 14:22,  2 users,  load average: 0.35, 0.26, 0.16

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -54,13 +54,13 @@ static int cmd_c(char *args) {
 
 static int cmd_q(char *args) {
   // -----
-  // static const uint32_t ebreak = 0x00100073;
-  // memcpy(guest_to_host(RESET_VECTOR), &ebreak, 4);
+  static const uint32_t ebreak = 0x00100073;
+  memcpy(guest_to_host(RESET_VECTOR), &ebreak, 4);
   // if(nemu_state.state)
   // {
 
   // }
-  // cpu_exec(0);
+  cpu_exec(1);
   // -----
   return -1;
 }
