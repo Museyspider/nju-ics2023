fa6b955e34d317e5ee47a719ff8ca6468a24bcd3
tracer-ics2023 <tracer@njuics.org>
2023-12-18 08:27:52 -0500

    >  compile NEMU QSX20210747 unian Linux unian-QiTianM415-D412 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:27:52 up  1:54,  2 users,  load average: 0.10, 0.16, 0.17

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -45,30 +45,30 @@ static void exec_once(Decode *s, vaddr_t pc) {
   s->snpc = pc;
   isa_exec_once(s);
   cpu.pc = s->dnpc;
-#ifdef CONFIG_ITRACE
-  char *p = s->logbuf;
-  p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
-  int ilen = s->snpc - s->pc;
-  int i;
-  uint8_t *inst = (uint8_t *)&s->isa.inst.val;
-  for (i = ilen - 1; i >= 0; i --) {
-    p += snprintf(p, 4, " %02x", inst[i]);
-  }
-  int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
-  int space_len = ilen_max - ilen;
-  if (space_len < 0) space_len = 0;
-  space_len = space_len * 3 + 1;
-  memset(p, ' ', space_len);
-  p += space_len;
+// #ifdef CONFIG_ITRACE
+//   char *p = s->logbuf;
+//   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
+//   int ilen = s->snpc - s->pc;
+//   int i;
+//   uint8_t *inst = (uint8_t *)&s->isa.inst.val;
+//   for (i = ilen - 1; i >= 0; i --) {
+//     p += snprintf(p, 4, " %02x", inst[i]);
+//   }
+//   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
+//   int space_len = ilen_max - ilen;
+//   if (space_len < 0) space_len = 0;
+//   space_len = space_len * 3 + 1;
+//   memset(p, ' ', space_len);
+//   p += space_len;
 
-#ifndef CONFIG_ISA_loongarch32r
-  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-  disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
-      MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
-#else
-  p[0] = '\0'; // the upstream llvm does not support loongarch32r
-#endif
-#endif
+// #ifndef CONFIG_ISA_loongarch32r
+//   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+//   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
+//       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
+// #else
+//   p[0] = '\0'; // the upstream llvm does not support loongarch32r
+// #endif
+// #endif
 }
 
 static void execute(uint64_t n) {
