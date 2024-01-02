026beeea45085beb3d3cf427db9a05f456cb2226
tracer-ics2023 <tracer@njuics.org>
2024-01-01 20:41:03 -0500

    >  compile NEMU QSX20210747 unian Linux unian-QiTianM415-D412 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:41:03 up 23:38,  2 users,  load average: 0.03, 0.09, 0.08

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -1,17 +1,17 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include <isa.h>
 
@@ -24,39 +24,53 @@
 #include <stdbool.h>
 
 extern int strtoval(char *);
-// --------- 
+// ---------
 
-enum {
-  TK_NOTYPE = 256, TK_EQ, TK_NUM, TK_PLUS, TK_MINUS, TK_MUL, TK_DIV, TK_L, TK_R, DEREF, TK_AND, TK_UNEQ,
-  TK_HEX, TK_REG, 
+enum
+{
+  TK_NOTYPE = 256,
+  TK_EQ,
+  TK_NUM,
+  TK_PLUS,
+  TK_MINUS,
+  TK_MUL,
+  TK_DIV,
+  TK_L,
+  TK_R,
+  DEREF,
+  TK_AND,
+  TK_UNEQ,
+  TK_HEX,
+  TK_REG,
 
   /* TODO: Add more token types */
 
 };
 
-static struct rule {
+static struct rule
+{
   const char *regex;
   int token_type;
 } rules[] = {
 
-  /* TODO: Add more rules.
-   * Pay attention to the precedence level of different rules.
-   */
+    /* TODO: Add more rules.
+     * Pay attention to the precedence level of different rules.
+     */
 
-  {" +", TK_NOTYPE},    // spaces
-  {"\\+", TK_PLUS},         // plus
-  {"==", TK_EQ},        // equal
+    {" +", TK_NOTYPE}, // spaces
+    {"\\+", TK_PLUS},  // plus
+    {"==", TK_EQ},     // equal
 
-  // -----------
-  {"\\-", TK_MINUS},
-  {"\\*", TK_MUL},
-  {"\\/", TK_DIV},
-  {"[0-9]+",  TK_NUM},
-  {"\\(", TK_L},
-  {"\\)", TK_R},
-  {"&&", TK_AND},
-  {"!=", TK_UNEQ},
-  // -----------
+    // -----------
+    {"\\-", TK_MINUS},
+    {"\\*", TK_MUL},
+    {"\\/", TK_DIV},
+    {"[0-9]+", TK_NUM},
+    {"\\(", TK_L},
+    {"\\)", TK_R},
+    {"&&", TK_AND},
+    {"!=", TK_UNEQ},
+    // -----------
 };
 
 #define NR_REGEX ARRLEN(rules)
@@ -66,42 +80,50 @@ static regex_t re[NR_REGEX] = {};
 /* Rules are used for many times.
  * Therefore we compile them only once before any usage.
  */
-void init_regex() {
+void init_regex()
+{
   int i;
   char error_msg[128];
   int ret;
 
-  for (i = 0; i < NR_REGEX; i ++) {
+  for (i = 0; i < NR_REGEX; i++)
+  {
     ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
-    if (ret != 0) {
+    if (ret != 0)
+    {
       regerror(ret, &re[i], error_msg, 128);
       panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
     }
   }
 }
 
-typedef struct token {
+typedef struct token
+{
   int type;
   char str[32];
 } Token;
 
 static Token tokens[32] __attribute__((used)) = {};
-static int nr_token __attribute__((used))  = 0;
+static int nr_token __attribute__((used)) = 0;
 
 // 识别token
-static bool make_token(char *e) {
+static bool make_token(char *e)
+{
   int position = 0;
   int i;
   regmatch_t pmatch;
 
   nr_token = 0;
 
-  while (e[position] != '\0') {
+  while (e[position] != '\0')
+  {
     /* Try all rules one by one. */
-    for (i = 0; i < NR_REGEX; i ++) {
+    for (i = 0; i < NR_REGEX; i++)
+    {
 
       // 这里它是确定了 一个要匹配的子串
-      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0)
+      {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
@@ -115,87 +137,90 @@ static bool make_token(char *e) {
          * of tokens, some extra actions should be performed.
          */
 
-        switch (rules[i].token_type) {
+        switch (rules[i].token_type)
+        {
 
-          // -------------
-          case TK_NOTYPE: 
-            break;
+        // -------------
+        case TK_NOTYPE:
+          break;
 
-          case TK_NUM: 
-            for(int i = 0; i < substr_len; i ++)
-            {
-              tokens[nr_token].str[i] = *(substr_start + i);
-            }
-            tokens[nr_token].type = rules[i].token_type;
-            nr_token++;
-            break;
+        case TK_NUM:
+          for (int i = 0; i < substr_len; i++)
+          {
+            tokens[nr_token].str[i] = *(substr_start + i);
+          }
+          tokens[nr_token].type = rules[i].token_type;
+          nr_token++;
+          break;
 
-          case TK_PLUS: 
-            tokens[nr_token].str[0] = *(substr_start);
-            tokens[nr_token].type = rules[i].token_type;
-            nr_token++;
-            break;
+        case TK_PLUS:
+          tokens[nr_token].str[0] = *(substr_start);
+          tokens[nr_token].type = rules[i].token_type;
+          nr_token++;
+          break;
 
-          case TK_MINUS: 
-            tokens[nr_token].str[0] = *(substr_start);
-            tokens[nr_token].type = rules[i].token_type;
-            nr_token++;
-            break;
+        case TK_MINUS:
+          tokens[nr_token].str[0] = *(substr_start);
+          tokens[nr_token].type = rules[i].token_type;
+          nr_token++;
+          break;
 
-          case TK_MUL: 
-            tokens[nr_token].str[0] = *(substr_start);
-            tokens[nr_token].type = rules[i].token_type;
-            nr_token++;
-            break;
-          
-          case TK_DIV: 
-            tokens[nr_token].str[0] = *(substr_start);
-            tokens[nr_token].type = rules[i].token_type;
-            nr_token++;
-            break;
+        case TK_MUL:
+          tokens[nr_token].str[0] = *(substr_start);
+          tokens[nr_token].type = rules[i].token_type;
+          nr_token++;
+          break;
 
-          case TK_R: 
-            tokens[nr_token].str[0] = *(substr_start);
-            tokens[nr_token].type = rules[i].token_type;
-            nr_token++;
-            break;
+        case TK_DIV:
+          tokens[nr_token].str[0] = *(substr_start);
+          tokens[nr_token].type = rules[i].token_type;
+          nr_token++;
+          break;
 
-          case TK_L: 
-            tokens[nr_token].str[0] = *(substr_start);
-            tokens[nr_token].type = rules[i].token_type;
-            nr_token++;
-            break;
-          
-          case TK_EQ: 
-            tokens[nr_token].str[0] = *(substr_start);
-            tokens[nr_token].str[1] = *(substr_start + 1);
-            tokens[nr_token].type = rules[i].token_type;
-            nr_token++;
-            break;
-          
-          case TK_UNEQ: 
-            tokens[nr_token].str[0] = *(substr_start);
-            tokens[nr_token].str[1] = *(substr_start + 1);
-            tokens[nr_token].type = rules[i].token_type;
-            nr_token++;
-            break;
-          
-          case TK_AND: 
-            tokens[nr_token].str[0] = *(substr_start);
-            tokens[nr_token].str[1] = *(substr_start + 1);
-            tokens[nr_token].type = rules[i].token_type;
-            nr_token++;
-            break;
+        case TK_R:
+          tokens[nr_token].str[0] = *(substr_start);
+          tokens[nr_token].type = rules[i].token_type;
+          nr_token++;
+          break;
 
-          // -------------
-          default: break;
+        case TK_L:
+          tokens[nr_token].str[0] = *(substr_start);
+          tokens[nr_token].type = rules[i].token_type;
+          nr_token++;
+          break;
+
+        case TK_EQ:
+          tokens[nr_token].str[0] = *(substr_start);
+          tokens[nr_token].str[1] = *(substr_start + 1);
+          tokens[nr_token].type = rules[i].token_type;
+          nr_token++;
+          break;
+
+        case TK_UNEQ:
+          tokens[nr_token].str[0] = *(substr_start);
+          tokens[nr_token].str[1] = *(substr_start + 1);
+          tokens[nr_token].type = rules[i].token_type;
+          nr_token++;
+          break;
+
+        case TK_AND:
+          tokens[nr_token].str[0] = *(substr_start);
+          tokens[nr_token].str[1] = *(substr_start + 1);
+          tokens[nr_token].type = rules[i].token_type;
+          nr_token++;
+          break;
+
+        // -------------
+        default:
+          break;
         }
 
         break;
       }
     }
 
-    if (i == NR_REGEX) {
+    if (i == NR_REGEX)
+    {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
     }
@@ -206,36 +231,36 @@ static bool make_token(char *e) {
 
 static bool check_parentheses(int p, int q)
 {
-  if(tokens[p].type != TK_L)
+  if (tokens[p].type != TK_L)
   {
     return false;
   }
   int L = 1;
   // int error = 0;
-  int count = 0;  // L 为0的次数只能是1次
-  for(int i = p + 1; i <= q; i ++)
+  int count = 0; // L 为0的次数只能是1次
+  for (int i = p + 1; i <= q; i++)
   {
     // printf("L=%d\n", L);
-    if(tokens[i].type==TK_L)
+    if (tokens[i].type == TK_L)
     {
-      L ++;
+      L++;
     }
-    if(tokens[i].type==TK_R)
+    if (tokens[i].type == TK_R)
     {
-      L --;
+      L--;
     }
-    if(L < 0)
+    if (L < 0)
     {
       // error = 1;
       printf("表达式错误！");
       return false;
     }
-    if(L == 0)
+    if (L == 0)
     {
       count++;
     }
   }
-  if(L == 0 && count == 1)
+  if (L == 0 && count == 1)
   {
     return true;
   }
@@ -249,38 +274,37 @@ static int optPosition(int p, int q)
   int level = 0;
   int pos = p;
   int L = 0;
-  for(int i = p; i <= q; i ++)
+  for (int i = p; i <= q; i++)
   {
-    if(tokens[i].type == TK_L)
+    if (tokens[i].type == TK_L)
     {
-      L ++;
+      L++;
     }
-    if(tokens[i].type == TK_R)
+    if (tokens[i].type == TK_R)
     {
-      L --;
+      L--;
     }
-    if(L == 0 && (tokens[i].type == TK_DIV || tokens[i].type == TK_MUL) && level == 0)
+    if (L == 0 && (tokens[i].type == TK_DIV || tokens[i].type == TK_MUL) && level == 0)
     {
       pos = i;
     }
-    if(L == 0 && (tokens[i].type == TK_PLUS || tokens[i].type == TK_MINUS))
+    if (L == 0 && (tokens[i].type == TK_PLUS || tokens[i].type == TK_MINUS))
     {
-      level ++;
+      level++;
       pos = i;
     }
   }
   return pos;
 }
 
-
 word_t eval(int p, int q)
 {
-  if (p > q) 
+  if (p > q)
   {
-    // Bad expression 
-    return 0;  // 是不是返回0
+    // Bad expression
+    return 0; // 是不是返回0
   }
-  else if (p == q) 
+  else if (p == q)
   {
     /* Single token.
      * For now this token should be a number.
@@ -288,30 +312,39 @@ word_t eval(int p, int q)
      */
     return strtoval(tokens[p].str);
   }
-  else if (check_parentheses(p, q) == true) {
+  else if (check_parentheses(p, q) == true)
+  {
     /* The expression is surrounded by a matched pair of parentheses.
      * If that is the case, just throw away the parentheses.
      */
     return eval(p + 1, q - 1);
   }
-  else {
+  else
+  {
     int op = optPosition(p, q);
     int val1 = eval(p, op - 1);
     int val2 = eval(op + 1, q);
 
-    switch (tokens[op].type) {
-      case TK_PLUS: return val1 + val2;
-      case TK_MINUS: return val1 - val2;
-      case TK_MUL: return val1 * val2;
-      case TK_DIV: return val1 / val2;
-      default: assert(0);
+    switch (tokens[op].type)
+    {
+    case TK_PLUS:
+      return val1 + val2;
+    case TK_MINUS:
+      return val1 - val2;
+    case TK_MUL:
+      return val1 * val2;
+    case TK_DIV:
+      return val1 / val2;
+    default:
+      assert(0);
     }
   }
 }
 
-
-word_t expr(char *e, bool *success) {
-  if (!make_token(e)) {
+word_t expr(char *e, bool *success)
+{
+  if (!make_token(e))
+  {
     *success = false;
     return 0;
   }
@@ -325,11 +358,9 @@ word_t expr(char *e, bool *success) {
 
   /* TODO: Insert codes to evaluate the expression. */
   // TODO();
-  for (int i = 0; i < nr_token; i ++) 
+  for (int i = 0; i < nr_token; i++)
   {
-    if (tokens[i].str[0] == '*' && (i == 0 || tokens[i - 1].type == TK_PLUS
-         || tokens[i - 1].type == TK_MINUS || tokens[i - 1].type == TK_MUL || tokens[i - 1].type == TK_DIV
-         || tokens[i - 1].type == DEREF ) ) 
+    if (tokens[i].str[0] == '*' && (i == 0 || tokens[i - 1].type == TK_PLUS || tokens[i - 1].type == TK_MINUS || tokens[i - 1].type == TK_MUL || tokens[i - 1].type == TK_DIV || tokens[i - 1].type == DEREF))
     {
       tokens[i].type = DEREF;
     }
@@ -338,7 +369,6 @@ word_t expr(char *e, bool *success) {
       tokens[i].type = TK_MUL;
     }
   }
-
 
   printf("expr=%d\n", eval(0, nr_token - 1));
 
