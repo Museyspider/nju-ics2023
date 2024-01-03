59b5c36075561afdfa271de45ac36eb7756c1888
tracer-ics2023 <tracer@njuics.org>
2024-01-02 20:42:01 -0500

    >  compile NEMU QSX20210747 unian Linux unian-QiTianM415-D412 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:42:01 up 1 day, 23:39,  2 users,  load average: 0.04, 0.12, 0.08

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -92,7 +92,7 @@ int watchpoint_val()
   WP *cur = head;
   while (cur != NULL)
   {
-    printf("cur->expr_addr=%d\n", cur->expr_addr);
+    printf("cur->expr_addr=%x\n", cur->expr_addr);
     if (cur->val != *((uint32_t *)(uint64_t)cur->expr_addr))
     {
       cur->val = *((uint32_t *)(uint64_t)cur->expr_addr);
