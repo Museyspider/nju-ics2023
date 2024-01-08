32447006324e5a9bc973d340234826b6d7be1d6b
QSX20210747-unian <2532815199@qq.com>
2024-01-08 09:30:47 -0500

    开始 指令章节

--- nemu/include/isa.h
+++ nemu/include/isa.h
@@ -1,17 +1,17 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #ifndef __ISA_H__
 #define __ISA_H__
@@ -21,9 +21,8 @@
 
 // The macro `__GUEST_ISA__` is defined in $(CFLAGS).
 // It will be expanded as "x86" or "mips32" ...
-typedef concat(__GUEST_ISA__, _CPU_state) CPU_state;    // riscv32_CPU_state  in <isa-def.h>
-typedef concat(__GUEST_ISA__, _ISADecodeInfo) ISADecodeInfo;
-
+typedef concat(__GUEST_ISA__, _CPU_state) CPU_state;         // riscv32_CPU_state  in <isa-def.h>
+typedef concat(__GUEST_ISA__, _ISADecodeInfo) ISADecodeInfo; // riscv32_ISADecodeInfo
 
 // monitor
 extern unsigned char isa_logo[];
@@ -39,9 +38,24 @@ struct Decode;
 int isa_exec_once(struct Decode *s);
 
 // memory
-enum { MMU_DIRECT, MMU_TRANSLATE, MMU_FAIL };
-enum { MEM_TYPE_IFETCH, MEM_TYPE_READ, MEM_TYPE_WRITE };
-enum { MEM_RET_OK, MEM_RET_FAIL, MEM_RET_CROSS_PAGE };
+enum
+{
+  MMU_DIRECT,
+  MMU_TRANSLATE,
+  MMU_FAIL
+};
+enum
+{
+  MEM_TYPE_IFETCH,
+  MEM_TYPE_READ,
+  MEM_TYPE_WRITE
+};
+enum
+{
+  MEM_RET_OK,
+  MEM_RET_FAIL,
+  MEM_RET_CROSS_PAGE
+};
 #ifndef isa_mmu_check
 int isa_mmu_check(vaddr_t vaddr, int len, int type);
 #endif
